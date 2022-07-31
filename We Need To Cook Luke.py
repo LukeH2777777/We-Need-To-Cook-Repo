434c70e35bdc4a879a05bafff2d4b6ea49e7522a
samhiorns <110356155+samhiorns@users.noreply.github.com>
2022-07-31 23:15:40 +0100

    Add files via upload

---
+++ We Need To Cook Luke.py
@@ -0,0 +1,39 @@
+def gamestart():
+    healthcount == "100"
+    healthcommand = "/health"
+    command, argument = 
+    mycommand.split()
+    print(command)/buy
+    print(argument)
+
+
+
+
+
+def tutorial():
+    import time
+    print("Welcome to the tutorial!")
+    time.sleep(2)
+    print("First off, lets set your health to 100!")
+    time.sleep(2)
+    gamestart()
+
+
+
+
+
+
+
+def startgame():
+    import time
+    print ("Welcome to We Need To Cook!")
+    time.sleep(2)
+    print("Would you like a tutorial?")
+    time.sleep(1)
+    print("Y or N")
+    tutorial=input()
+    if tutorial == "y":
+        tutorial()
+    if tutorial == "Y":
+        tutorial()
+
