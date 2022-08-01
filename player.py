5248f443304cb57d84886ff75a67a67ba1569fa6
William Halliday <58829019+wvadhy@users.noreply.github.com>
2022-08-01 18:32:27 +0700

    Update player.py

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 from inventory import *
 
+
 class Player:
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
         self.__inventory = Inventory.get_guns()["Machete"]
         self.xp = 0
         self.health = 100
         self.level = 0
 
-    def get_inventory(self):
+    def get_inventory(self) -> dict:
         return self.__inventory
