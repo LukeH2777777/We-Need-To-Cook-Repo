a85248f8435c00885d1b6cd6ee38fa7502523384
William Halliday <58829019+wvadhy@users.noreply.github.com>
2022-08-01 18:31:22 +0700

    Update player.py

--- player.py
+++ player.py
@@ -1,19 +1,13 @@
-from random import randint
+from inventory import *
 
-class Player():
-    name = ""
-    health = 100
-    xp = 0
-    seller_level = 0
-    weapon = "machete"
+class Player:
 
     def __init__(self, name: str):
         self.name = name
-    
-    def getName(self) -> str:
-        return self.name
+        self.__inventory = Inventory.get_guns()["Machete"]
+        self.xp = 0
+        self.health = 100
+        self.level = 0
 
-    def getAttackDamage(self):
-        weapons = {"machete": [45, 55]} # TODO: move this somewhere else, and expand to all weapons
-        return randint(weapons[self.weapon][0], weapons[self.weapon][1])
-
+    def get_inventory(self):
+        return self.__inventory
