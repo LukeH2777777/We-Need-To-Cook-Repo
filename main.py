9f559143f66adfdb814104448f617c06347e2f71
William Halliday <58829019+wvadhy@users.noreply.github.com>
2022-08-01 18:41:26 +0700

    main file cus no one added one
    Barebones, feel free to fork and change

---
+++ main.py
@@ -0,0 +1,13 @@
+from Player import *
+from npc import *
+
+player = Player("will")
+dealer = Dealer("john")
+
+commands = {
+    "inventory": player.get_inventory(),
+    "drugs": dealer.get_stock()
+}
+
+print(player.get_inventory())
+print(dealer.get_stock())
