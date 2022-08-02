3d85ec22e36b777264204211b6a416ccd7edc11f
PunchCakee <email@example.com>
2022-08-02 09:03:44 +0300

    cleaned up some poitnless files

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from player import *
 from npc import *
+import time
+import utilities as ut
+import konami
 
 player = Player("will")
 dealer = Dealer("john")
@@ -9,5 +12,37 @@ commands = {
     "drugs": dealer.get_stock()
 }
 
-print(player.get_inventory())
-print(dealer.get_stock())
+
+def gamestart():
+    command, argument = input().split()
+    print(f"{command=}")
+    print(f"{argument=}")
+
+# TODO: do this function.
+def parseCommand():
+    pass
+
+def tutorial():
+    ut.printout("Welcome to the tutorial!", delay=2)
+    ut.printout("First off, lets set your health to 100!", delay=2)
+    gamestart()
+
+
+# TODO: see if a save file exists, if so, load the file, else do this part.
+def startgame():
+    ut.printout("Welcome to We Need To Cook!", delay=2)
+    ut.printout("Would you like a tutorial?", delay=1)
+    ut.printout("Y or N")
+    if input().lower() in ["y", "yes"]:
+        tutorial()
+
+
+#player = Player("Young lad")
+#print(f"{player.getName()} swung for {player.getAttackDamage()} damage")
+
+try:
+    startgame()
+except KeyboardInterrupt:
+    print("Quitting....")
+    #if you want to save stuff when the player quits
+    #put your code here
