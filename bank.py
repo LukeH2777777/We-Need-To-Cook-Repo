344fcbc9e4916228309dc04316f71e94a3db27a7
PunchCakee <email@example.com>
2022-08-03 11:06:19 +0300

    Added cash system so money doesnt come from thin air.

--- bank.py
+++ bank.py
@@ -6,38 +6,50 @@ Bank = Bank()
 
 or else it would throw a metric fuck tone of errors :)
 
-To deposit:
-Bank.deposit(1000)
-
-To withdraw:
-Bank.withdraw(1000)
-
-To check balance:
-Bank.balance()
-
 """
 
 
 class Bank:
 
-    def __init__(self,money=1000):
+    def __init__(self,money=0,cash=1000):
         self.money = money
+        self.cash = cash
 
-    def deposit(self,amount):
-        self.money += amount
-        ut.printout(f"{amount} has been credited to your account.")
-
-    def withdraw(self,amount):
-        if amount > self.money:
-            print("insufficient balance")
-        else:
-            self.money -= amount
-            ut.printout(f"{amount} has been withdrawn from your account.")
-    
+    def cash_balance(self):
+        ut.printout(f"you got {self.cash} in cash")
+      
     def balance(self):
         ut.printout(f"your balance is {self.money}.")
 
+    def deposit(self,amount):
+        if amount > self.cash:
+            ut.printout("insufficient funds")
+        else:
+            self.money += amount
+            self.cash -= amount
+            ut.printout(f"{amount} has been credited to your account.")
+
+    def withdraw(self,amount):
+        if amount > self.money:
+            print("insufficient funds")
+        else:
+            self.money -= amount
+            self.money += self.cash
+            ut.printout(f"{amount} has been withdrawn from your account.")
+    
     def to_dict(self):
         return {
-            "Bank": self.money
+            "Bank": self.money,
+            "Cash": self.cash
         }
+
+
+
+#for testing remove on release.
+"""
+Bank = Bank()
+Bank.balance()
+Bank.cash_balance()
+Bank.withdraw(1000)
+Bank.deposit(10000000)
+"""
