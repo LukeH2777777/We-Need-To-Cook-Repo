4b66d8bb794ae9a9d12808ab26637c229bffe2de
PunchCakee <email@example.com>
2022-09-13 06:20:27 +0300

    added vito boss and test cases for him

--- test_vito.py
+++ test_vito.py
@@ -1,20 +1,16 @@
-import unittest
 from bosses import Vito_ganyada
 
-class VitoGanyadaTesting(unittest.TestCase):
     
-    def test_decrement(self):
-        vito = Vito_ganyada()
-        vito.decrement(100)
-        result = vito.score
-        self.assertEqual(result, 400)
+def test_decrement():
+    vito = Vito_ganyada()
+    vito.decrement(100)
+    result = vito.score
+    assert result == 400
 
-    def test_increment(self):
-        vito = Vito_ganyada()
-        vito.increment(100)
-        result = vito.score
-        self.assertEqual(result, 600)
+def test_increment():
+    vito = Vito_ganyada()
+    vito.increment(100)
+    result = vito.score
+    assert result == 600
 
 
-if __name__ == '__main__':
-    unittest.main()
