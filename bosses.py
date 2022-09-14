b1850f22bf2e6627f4477279d17371bf8057045c
PunchCakee <email@example.com>
2022-09-14 09:39:52 +0300

    Removed some test code in bosses.py that i forgot to remove

--- bosses.py
+++ bosses.py
@@ -5,16 +5,9 @@ class Vito_ganyada(Health):
     
     def __init__(self):
         self.score = 500
-    
+
     def current_health(self):
         return self.score
 
     def to_dict(self):
         return super().to_dict()
-
-
-
-
-
-vito = Vito_ganyada()
-print(vito.current_health())
